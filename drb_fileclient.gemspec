bde56b15b4b70a929543131a156743b5e3f409e4
Your Name <you@example.com>
2018-01-22 22:10:56 +0000

    1st commit

---
+++ drb_fileclient.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name = 'drb_fileclient'
+  s.version = '0.1.0'
+  s.summary = 'Reads or writes files from a remote DRb server.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/drb_fileclient.rb']
+  s.signing_key = '../privatekeys/drb_fileclient.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/drb_fileclient'
+end
---
+++ lib/drb_fileclient.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+# file: drb_fileclient.rb
+
+require 'drb'
+
+
+class DRbFileClient
+
+  def initialize(host: 'localhost', port: '61010')
+    DRb.start_service
+
+    # attach to the DRb server via a URI given on the command line
+    @file = DRbObject.new nil, "druby://#{host}:#{port}"
+    
+  end
+
+  def read(filename)
+    
+    @file.read filename
+    
+  end
+  
+  def write(filename, s)
+    
+    @file.write filename, s
+    
+  end  
+
+end 
+
