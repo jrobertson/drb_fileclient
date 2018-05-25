1096c503829ddd5d6d9d4d9997c6cbff9af8c1ab
Your Name <you@example.com>
2018-05-25 23:55:02 +0100

    DRbFileClient::exists? can now accept the filepath prefixed by the dfs protocol and dfs host address.

--- lib/drb_fileclient.rb
+++ lib/drb_fileclient.rb
@@ -7,28 +7,50 @@ require 'drb'
 
 class DRbFileClient
 
-  def initialize(host: 'localhost', port: '61010')
+  def initialize(location=nil, host: nil, port: '61010')
+    
+    if location then
+      
+      host = location[/(?<=^dfs:\/\/)[^\/:]+/]
+      port = location[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
+      @filename = location[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]
+      
+    end
     
     DRb.start_service
 
     # attach to the DRb server via a URI given on the command line
-    @file = DRbObject.new nil, "druby://#{host}:#{port}"
+    @file = DRbObject.new nil, "druby://#{host}:#{port}" if host
     
   end
   
-  def exists?(filename)
+  def exists?(filename=@filename)
+  
+    filename2 = if @file then
+      
+      filename
+      
+    else
+      
+      host = filename[/(?<=^dfs:\/\/)[^\/:]+/]
+      port = filename[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
+
+      @file = DRbObject.new nil, "druby://#{host}:#{port}"
+      filename[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]      
+      
+    end
     
-    @file.exists? filename
+    @file.exists? filename2
     
   end
 
-  def read(filename)
+  def read(filename=@filename)
     
     @file.read filename
     
   end
   
-  def write(filename, s)
+  def write(filename=@filename, s)
     
     @file.write filename, s
     
