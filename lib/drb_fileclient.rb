08c395f8c103365d8d50475fc328511d284c477e
James Robertson <james@jamesrobertson.eu>
2018-08-11 11:40:58 +0100

    feature: Added public methods *mkdir*, *mkdir_p*, *chdir*, and *pwd*.

--- lib/drb_fileclient.rb
+++ lib/drb_fileclient.rb
@@ -13,72 +13,127 @@ class DRbFileClient
       
       host = location[/(?<=^dfs:\/\/)[^\/:]+/]
       port = location[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
-      @filename = location[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]
+      @directory = location[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]
       
     end
     
     DRb.start_service
-
-    # attach to the DRb server via a URI given on the command line
-    @file = DRbObject.new nil, "druby://#{host}:#{port}" if host
     
   end
+  
+  def chdir(raw_path)
+    
+    directory = if raw_path[0] == '/'  then
+      raw_path[1..-1]
+    elsif raw_path =~ /^dfs:\/\//
+      parse_path(raw_path)
+    else      
+      File.join(@directory, raw_path)
+    end    
+    
+    if @file.exists? directory then
+      @directory = directory
+    else
+      'No such file or directory'
+    end
+    
+  end  
   
   def exists?(filename=@filename)  
     
-    filename2 = parse_path(filename)
-    @file.exists? filename2
-    
-  end
+    filename2 = if filename =~ /^dfs:\/\// then
+      parse_path(filename)
+    else
+      filename
+    end
 
-  def read(filename=@filename)
-    
-    filename2 = parse_path(filename)
-    @file.read filename2
+    @file.exists? File.join(@directory, filename2)
     
   end
   
-  def write(filename=@filename, s)
+  def mkdir(name)
+    path = parse_path(name)
+    @file.mkdir path
+  end
+  
+  def mkdir_p(raw_path)
+    path = parse_path(raw_path)
+    @file.mkdir_p path
+  end  
+  
+  def pwd()
     
-    filename2 = parse_path(filename)
-    @file.write filename2, s
+    '/' + @directory if @file
+    
+  end  
+
+  def read(filename=@filename)
+    
+    path = if filename =~ /^dfs:\/\// then
+      parse_path(filename)
+    else
+      File.join(@directory, filename)
+    end
+    
+    @file.read path
+  end
+  
+  def write(filename=@filename, s)
+        
+    path = if filename =~ /^dfs:\/\// then
+      parse_path(filename)
+    else
+      File.join(@directory, filename)
+    end
+    
+    @file.write path, s     
     
   end
 
   private
   
   def parse_path(filename)
-    
-    if @file then
-      
-      filename
-      
-    else
-      
-      host = filename[/(?<=^dfs:\/\/)[^\/:]+/]
-      port = filename[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
 
-      @file = DRbObject.new nil, "druby://#{host}:#{port}"
-      filename[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]      
-      
-    end    
-    
+    host = filename[/(?<=^dfs:\/\/)[^\/:]+/]
+    port = filename[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
+
+    @file = DRbObject.new nil, "druby://#{host}:#{port}"
+    filename[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]          
+
   end
 
 end 
 
 class DfsFile
   
+  @client = DRbFileClient.new
+  
   def self.exists?(filename)
-    DRbFileClient.new(filename).exists?
+    @client.exists?(filename)
   end
   
+  def self.chdir(path)
+    @client.chdir(path)
+  end    
+    
+  def self.mkdir(name)
+    @client.mkdir(name)
+  end  
+  
+  def self.mkdir_p(path)
+    @client.mkdir_p(path)
+  end       
+  
+  def self.pwd()
+    @client.pwd()
+  end     
+  
   def self.read(filename)
-    DRbFileClient.new.read(filename)
+    @client.read(filename)
   end
   
   def self.write(filename, s)
-    DRbFileClient.new.write(filename, s)
+    @client.write(filename, s)
   end
   
 end
